ca4584668953fc7c66eeda1f9316cc30b09056e0
jj <jj@ondemandenv.dev>
2025-03-05 21:16:16 -0500

    repo grant not working with Repository.from ...
    moved to contracts ... + new img

--- src/main/java/com/bezkoder/spring/swagger/SpringBootSwagger3ExampleApplication.java
+++ src/main/java/com/bezkoder/spring/swagger/SpringBootSwagger3ExampleApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSwagger3ExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSwagger3ExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSwagger3ExampleApplication.class, args);
+        System.out.println("trigger building new img");
+    }
 
 }
